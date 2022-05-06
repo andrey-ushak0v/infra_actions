b4cbf23fa99a6e648c424acee60bea93746c0712
Ушаков <andrey.ushakov.00@inbox.ru>
2022-05-06 13:09:29 +0300

    test flake8

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
+
 ]
